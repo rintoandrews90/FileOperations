30f32114a46e9d3b2fe94e72e8b601216f3f1439
Rinto Andrews <rintoandrews90@gmail.com>
2019-07-28 14:04:06 +0530

    Abstract Functions Added

---
+++ FileOperations/FileOperations+Directories.swift
@@ -0,0 +1,9 @@
+//
+//  FileOperations+Directories.swift
+//  FileOperations
+//
+//  Created by Rinto Andrews on 28/07/19.
+//  Copyright © 2019 Rinto Andrews. All rights reserved.
+//
+
+import Foundation
