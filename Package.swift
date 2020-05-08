29eff35219f75e51ef96394c1bbac1917ce593ac
rintoandrews90 <rintoandrews90@gmail.com>
2020-05-08 22:05:02 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/rintoandrews90/FileOperations.git", from: "0.0.8"),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
