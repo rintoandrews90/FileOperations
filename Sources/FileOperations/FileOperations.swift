c660633588c47eae78dbd723ef62a93a89a3e74f
Rinto Andrews <rintoandrews90@gmail.com>
2020-05-08 22:14:14 +0530

    # Issue Fix

--- Sources/FileOperations/FileOperations.swift
+++ Sources/FileOperations/FileOperations.swift
@@ -1,3 +1,17 @@
-struct FileOperations {
-    var text = "Hello, World!"
+
+public class FileOperations {
+
+    fileprivate init() { }
+
+    /// DirectoryPath define type of directory in application sandbox
+    ///
+    /// - document: document Directory
+    /// - temp: temp Directory
+    /// - cache: cache Directory
+    public enum DirectoryPath {
+        case document
+        case temp
+        case cache
+    }
+
 }
