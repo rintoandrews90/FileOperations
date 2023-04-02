8160835c983e9a19a43c9ff691ce26dcd834c184
Rinto Andrews <rintoandrews90@gmail.com>
2023-04-02 18:57:57 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "FileOperations",path: "Sources"
+            name: "FileOperations",path: "Sources/FileOperations"
             dependencies: []),
         .testTarget(
             name: "FileOperationsTests",
