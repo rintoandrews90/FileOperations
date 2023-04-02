ec813df5727fe310669b9aac3374ad1e22db2c42
Rinto Andrews <rintoandrews90@gmail.com>
2023-04-02 22:01:42 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FileOperations",
-             exclude: ["FileOperations". "FileOperationsExample"]
+            exclude: ["FileOperations". "FileOperationsExample"],
             dependencies: []),
     ]
 )
