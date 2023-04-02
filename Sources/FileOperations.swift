d8f96535d970bc83a9a1a29a9de2fca0aa82bc29
Rinto Andrews <rintoandrews90@gmail.com>
2023-04-02 21:54:07 +0300

    fix

---
+++ Sources/FileOperations.swift
@@ -0,0 +1,26 @@
+//
+//  FileOperations.swift
+//  FileOperations
+//
+//  Created by Rinto Andrews on 28/07/19.
+//  Copyright © 2019 Rinto Andrews. All rights reserved.
+//
+
+import Foundation
+
+public class FileOperations {
+
+    fileprivate init() { }
+
+    /// DirectoryPath define type of directory in application sandbox
+    ///
+    /// - document: document Directory
+    /// - temp: temp Directory
+    /// - cache: cache Directory
+    public enum DirectoryPath {
+        case document
+        case temp
+        case cache
+    }
+
+}
